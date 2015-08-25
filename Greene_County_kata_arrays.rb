7381d42fc6653d52806e58bd63357fc361ce9366
mrsbuddhaman <mrs.buddhaman@gmail.com>
2015-08-24 20:21:35 -0400

    Update Greene_County_kata_arrays.rb

--- Greene_County_kata_arrays.rb
+++ Greene_County_kata_arrays.rb
@@ -1,12 +1,14 @@
 count = 1
 numbers = []
 100.times do
-	if count % 3 == 0
+	if count % 3 == 0 && count % 5 == 0
+	numbers << "Greene County"
+	elsif count % 3 == 0
 	numbers << "Greene"
-        elsif
-        count % 5 == 0
-        numbers << "County"
-        else
+	elsif
+	count % 5 == 0
+	numbers << "County"
+	else
 	numbers << (count)
 	end
 	count += 1
